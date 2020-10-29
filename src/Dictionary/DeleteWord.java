4b0b19405db775f6ceb79152dd8935e5c1590eb5
bhnbh1999 <48500166+bhnbh1999@users.noreply.github.com>
2020-10-30 03:41:36 +0700

    Update DeleteWord.java

--- src/Dictionary/DeleteWord.java
+++ src/Dictionary/DeleteWord.java
@@ -1,25 +1,18 @@
 package Dictionary;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class DeleteWord {
-
-    public void deleteWord(String word_target){
+    public void deleteWord(String word_target) {
         Connection connection = DBConnection.KetNoi();
-        String sql = "delete from tbl_edict where word = ?";
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Nhập từ cần xóa: ");
-//        String word_target = scanner.nextLine();
-
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-            preparedStatement.setString(1,word_target);
+            PreparedStatement preparedStatement = connection.prepareStatement("delete from tbl_edict where word = ?");
+            preparedStatement.setString(1, word_target);
             preparedStatement.executeUpdate();
-
             preparedStatement.close();
             connection.close();
-
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
