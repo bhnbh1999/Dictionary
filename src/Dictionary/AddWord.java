fde01aac5f75aa51a8275e5eccf2723583281258
bhnbh1999 <48500166+bhnbh1999@users.noreply.github.com>
2020-10-30 03:41:32 +0700

    Update AddWord.java

--- src/Dictionary/AddWord.java
+++ src/Dictionary/AddWord.java
@@ -1,31 +1,19 @@
 package Dictionary;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-public class AddWord{
-
-
-    public void addWord(String word_target,String word_explain){
-        Connection connection= DBConnection.KetNoi();
-
-//        String word_target;
-//        String word_explain;
-//        System.out.print("Nhập từ: ");
-//        word_target = scanner.nextLine();
-//        System.out.print("Nhập nghĩa: ");
-//        word_explain = scanner.nextLine();
-        String sql = "insert into tbl_edict(word,detail) values (?,?)";
+public class AddWord {
+    public void addWord(String word_target, String word_explain) {
+        Connection connection = DBConnection.KetNoi();
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1,word_target);
-            preparedStatement.setString(2,word_explain);
-
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into tbl_edict(word,detail) values (?,?)");
+            preparedStatement.setString(1, word_target);
+            preparedStatement.setString(2, word_explain);
             preparedStatement.executeUpdate();
-
-
             preparedStatement.close();
             connection.close();
-
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
