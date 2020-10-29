701a95486ef26b2e8ebb80959e3d958cd7947cd2
bhnbh1999 <48500166+bhnbh1999@users.noreply.github.com>
2020-10-30 03:41:54 +0700

    Update ControllerAdd.java

--- src/JavaFx/ControllerAdd.java
+++ src/JavaFx/ControllerAdd.java
@@ -8,23 +8,24 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class ControllerAdd {
+    @FXML
+    private TextField textEnglish;
+    @FXML
+    private TextField textVN;
+    @FXML
+    private Button add;
 
-    @FXML private TextField textEnglish;
-    @FXML private TextField textVN;
-    @FXML private Button add;
-
-    public void addtoWord(ActionEvent event) {
+    public void addToWord(ActionEvent event) {
         if (!textEnglish.getText().equals("") && !textVN.getText().equals("")) {
             AddWord addWord = new AddWord();
-            addWord.addWord(textEnglish.getText(),textVN.getText());
+            addWord.addWord(textEnglish.getText(), textVN.getText());
             textEnglish.clear();
             textVN.clear();
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Congratulations");
             alert.setContentText("Bạn đã thêm thành công!");
             alert.show();
-        }
-        else{
+        } else {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("ERROR");
             alert.setContentText("Mời nhập từ");
