94b05b4ed479803ec1dc29fbc51a0a1511a237b2
bhnbh1999 <48500166+bhnbh1999@users.noreply.github.com>
2020-10-30 03:41:40 +0700

    Update EditWord.java

--- src/Dictionary/EditWord.java
+++ src/Dictionary/EditWord.java
@@ -1,27 +1,18 @@
 package Dictionary;
+
 import java.sql.*;
+
 public class EditWord {
 
     public void editWord(String word_target,String word_explain){
         Connection connection= DBConnection.KetNoi();
-
-//        String word_target;
-//        String word_explain;
-//        System.out.print("Nhập từ: ");
-//        word_target = scanner.nextLine();
-//        System.out.print("Nhập nghĩa: ");
-//        word_explain = scanner.nextLine();
-        String sql = "update tbl_edict set detail =? where word = ?";
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
+            PreparedStatement preparedStatement = connection.prepareStatement("update tbl_edict set detail =? where word = ?");
             preparedStatement.setString(2,word_target);
             preparedStatement.setString(1,word_explain);
-
             preparedStatement.executeUpdate();
             preparedStatement.close();
             connection.close();
-
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
