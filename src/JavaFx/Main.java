6aaaf4dd82539cbe74fa0d86e896b449dd9400b7
bhnbh1999 <48500166+bhnbh1999@users.noreply.github.com>
2020-10-30 03:42:06 +0700

    Update Main.java

--- src/JavaFx/Main.java
+++ src/JavaFx/Main.java
@@ -7,14 +7,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-
         try {
             Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
             primaryStage.setTitle("Dictionary");
