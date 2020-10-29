a2ad52adc3ebb8814354824cd563bc6e1f0cb7b4
bhnbh1999 <48500166+bhnbh1999@users.noreply.github.com>
2020-10-30 03:41:38 +0700

    Update DisplayInfo.java

--- src/Dictionary/DisplayInfo.java
+++ src/Dictionary/DisplayInfo.java
@@ -1,81 +1,46 @@
 package Dictionary;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
-public class DisplayInfo{
-
+public class DisplayInfo {
     Connection connection = null;
     PreparedStatement preparedStatement = null;
     DBConnection dbConnection = new DBConnection();
 
-    public void Display(){
-        connection = dbConnection.KetNoi();
-
-        String sql = "select * from tbl_edict";
-
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
-                String word_target = resultSet.getString("word");
-                String word_explain = resultSet.getString("detail");
-
-
-                System.out.println(word_target + "\t" + word_explain);
-            }
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-    public String lookUp(String word_english){
-        connection = dbConnection.KetNoi();
-
-//        System.out.print("Nhập từ cần tìm: ");
-//        Scanner scanner = new Scanner(System.in);
-//        String word = scanner.nextLine();
+    public String lookUp(String word_english) {
+        connection = DBConnection.KetNoi();
         String test = "";
         String sql = "select * from tbl_edict";
-
         try {
             preparedStatement = connection.prepareStatement(sql);
-
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 String word_target = resultSet.getString("word");
                 String word_explain = resultSet.getString("detail");
-                if (word_english.equals(word_target)){
-                   // System.out.println(word_target + "\t" + word_explain);
-                    test =  word_explain;
+                if (word_english.equals(word_target)) {
+                    test = word_explain;
                 }
             }
-
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return test;
     }
-    public ArrayList<String> Searcher(String word_english){
+
+    public ArrayList<String> Searcher(String word_english) {
         connection = dbConnection.KetNoi();
         ArrayList<String> word_ = new ArrayList<>();
-
-//        System.out.print("Nhập từ cần tìm: ");
-//        Scanner scanner = new Scanner(System.in);
-//        String word = scanner.nextLine();
         String sql = "select word from tbl_edict";
-
         try {
             preparedStatement = connection.prepareStatement(sql);
-
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 String word_target = resultSet.getString("word");
-                //String word_explain = resultSet.getString("detail");
-                if (word_target.startsWith(word_english)){
-                    //System.out.println(word_target + "\t" + word_explain);
+                if (word_target.startsWith(word_english)) {
                     word_.add(word_target);
                 }
             }
@@ -84,5 +49,4 @@ public class DisplayInfo{
         }
         return word_;
     }
-
 }
