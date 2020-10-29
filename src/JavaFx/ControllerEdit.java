ad4ae04f4fe3b3cdb3f652d483e2a9cf1e885200
bhnbh1999 <48500166+bhnbh1999@users.noreply.github.com>
2020-10-30 03:42:00 +0700

    Update ControllerEdit.java

--- src/JavaFx/ControllerEdit.java
+++ src/JavaFx/ControllerEdit.java
@@ -1,6 +1,5 @@
 package JavaFx;
 
-import Dictionary.AddWord;
 import Dictionary.EditWord;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,21 +10,22 @@ import javafx.scene.control.TextField;
 public class ControllerEdit {
     @FXML
     private TextField textEnglish;
-    @FXML private TextField textVN;
-    @FXML private Button edit;
+    @FXML
+    private TextField textVN;
+    @FXML
+    private Button edit;
 
     public void editWord(ActionEvent event) {
         if (!textEnglish.getText().equals("") && !textVN.getText().equals("")) {
             EditWord edit = new EditWord();
-            edit.editWord(textEnglish.getText(),textVN.getText());
+            edit.editWord(textEnglish.getText(), textVN.getText());
             textEnglish.clear();
             textVN.clear();
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Congratulations");
             alert.setContentText("Bạn đã sửa thành công!");
             alert.show();
-        }
-        else{
+        } else {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("ERROR");
             alert.setContentText("Mời nhập từ");
