b2a9de4a47a10c90daa65a60f69bb6d66168ed49
bhnbh1999 <48500166+bhnbh1999@users.noreply.github.com>
2020-10-30 03:41:42 +0700

    Update Main.java

--- src/Dictionary/Main.java
+++ src/Dictionary/Main.java
@@ -1,16 +1,5 @@
 package Dictionary;
 
 public class Main {
-
-
-    public static void main(String[] args) {
-//        DBConnection dbConnection = new DBConnection();
-//        dbConnection.KetNoi();
-//        AddWord add = new AddWord();
-//        add.addWord();
-       // DisplayInfo displayInfo = new DisplayInfo();
-        //displayInfo.lookUp();
-        //displayInfo.Display();
-        //displayInfo.Searcher();
-    }
+    public static void main(String[] args) {}
 }
